b00bbbfd6ef34056cf124f6a67dffdece57d485d
AnnahGerletti <agerletti@gmail.com>
2018-02-26 12:01:11 +1300

    edited conditional

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,21 +1,20 @@
 var TwoFer = function () {}
 
+var who = ''
 TwoFer.prototype.twoFer = function (who) {
-  var who = ''
-      if (who !== 'Bob' || 'Alice') {
-        return 'One for you, one for me.'
-      } else if (who === 'Alice') {
-        return 'One for Alice, one for me.'
-      } else {
-        return 'One for Bob, one for me.'
-      }
-    }
-  // your code goes here
-  // You will have to use the parameter who
-  // in some way. In this example, it is just
-  // returned, but your solution will have to
-  // use a conditional.
-  console.log(who)
-};
+  if (who !== 'Bob' || 'Alice') {
+    return 'One for you, one for me.'
+  } else if (who === 'Alice') {
+    return 'One for Alice, one for me.'
+  } else {
+    return 'One for Bob, one for me.'
+  }
+}
+// your code goes here
+// You will have to use the parameter who
+// in some way. In this example, it is just
+// returned, but your solution will have to
+// use a conditional.
+TwoFer(who)
 
 module.exports = TwoFer
