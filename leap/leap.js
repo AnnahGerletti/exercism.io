8ac1e16d9ab748a9a32d20a92c34d6be0260021a
AnnahGerletti <agerletti@gmail.com>
2018-02-27 09:13:12 +1300

    use modules

---
+++ leap/leap.js
@@ -0,0 +1,30 @@
+//
+// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Year = function () {
+//
+// YOUR CODE GOES HERE
+//
+};
+
+Year.prototype.isLeap = function () {
+//
+// YOUR CODE GOES HERE
+//
+};
+
+module.exports = Year;
+
+var year = Number 
+
+if (year % 4 === 0 ){
+  return "Is Leap Year"
+}
+if else (year % 100 || 400 === 0){
+  return "not a leap year"
+}
+else {
+  return "Nope"
+}
