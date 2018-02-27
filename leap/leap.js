dd79b2d21a78ff3b8b69ef8d20922b3ea4e511eb
AnnahGerletti <agerletti@gmail.com>
2018-02-27 17:33:45 +1300

    boolean and % practice

--- leap/leap.js
+++ leap/leap.js
@@ -8,14 +8,24 @@ var Year = function (input) {
 }
 
 Year.prototype.isLeap = function () {
-  if (this.year % 4 === 0) {
+  if (this.year % 400 === 0) {
     return true
-  } else if (this.year % 100 || 400 === 0) {
+  } else if (this.year % 100 === 0) {
     return false
+  } else if (this.year % 4 === 0) {
+    return true
   } else {
     return false
   }
-
 }
+  module.exports = Year
+  
 
-module.exports = Year
+                  //   if (this.year % 100 === 0 && this.year % 400 !== 0) {
+                  //     return false
+                  //   } else if (this.year % 4 === 0) {
+                  //     return true
+                  //   } else {
+                  //     return false
+                  //   }
+                  // }
