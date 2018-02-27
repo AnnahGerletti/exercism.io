60a4211ddb2231b2d687143aa528ad7d7ff2fe0a
AnnahGerletti <agerletti@gmail.com>
2018-02-27 21:06:03 +1300

    fixed typo

--- leap/leap.js
+++ leap/leap.js
@@ -18,14 +18,12 @@ Year.prototype.isLeap = function () {
     return false
   }
 }
-  module.exports = Year
-  
-
-                  //   if (this.year % 100 === 0 && this.year % 400 !== 0) {
-                  //     return false
-                  //   } else if (this.year % 4 === 0) {
-                  //     return true
-                  //   } else {
-                  //     return false
-                  //   }
-                  // }
+module.exports = Year
+//   if (this.year % 100 === 0 && this.year % 400 !== 0) {
+//     return false
+//   } else if (this.year % 4 === 0) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
